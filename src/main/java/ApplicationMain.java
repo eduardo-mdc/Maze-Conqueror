285dc4e5be24545ea07e4264aad28d404f4df843
José Carvalho <z.m.f.carvalho@gmail.com>
2021-12-30 15:57:52 +0000

    load_walls() added
    generates a wall to encapsulate the map

--- src/main/java/ApplicationMain.java
+++ src/main/java/ApplicationMain.java
@@ -5,7 +5,7 @@ import element.*;
 
 public class ApplicationMain {
     public static void main(String[] args) {
-        Maze maze = new Maze(10);
+        Maze maze = new Maze(5);
         System.out.println(maze.stringMaze());
     }
 }
--- src/main/java/maze/Maze.java
+++ src/main/java/maze/Maze.java
@@ -5,11 +5,20 @@ import java.util.Arrays;
 public class Maze {
     private int[][] maze;
     public Maze(int dim){
-        MazeGenerator gen = new MazeGenerator(dim);
+        MazeGenerator gen = new MazeGenerator(dim-1);
         gen.generateMaze();
-        maze = gen.getIntMaze();
-    }
+        maze = load_walls(gen.getIntMaze(),dim);
 
+    }
+    static private int[][] load_walls(int[][] map , int dim){
+        int[][] maze = new int [dim][dim];
+        for(int i = 1 ; i < dim-1; i++ ){
+            for (int j = 1; j < dim-1;j++) {
+                if (map[i-1][j-1] != 0 )maze[i][j] = map[i-1][j-1];
+            }
+        }
+        return maze;
+    }
     public String stringMaze() {
         StringBuilder sb = new StringBuilder();
         for (int[] row : maze) {
