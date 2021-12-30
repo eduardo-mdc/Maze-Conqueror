b12b6ad5dbe22aa8bad46c1eda5fa5fa1eb09fed
José Carvalho <z.m.f.carvalho@gmail.com>
2021-12-30 16:17:43 +0000

    Update Maze.java

--- src/main/java/maze/Maze.java
+++ src/main/java/maze/Maze.java
@@ -2,16 +2,23 @@ package maze;
 
 import java.util.Arrays;
 
-public class Maze{
-
+public class Maze {
     private int[][] maze;
-
-    public Maze(int dim) {
-        MazeGenerator gen = new MazeGenerator(dim);
+    public Maze(int dim){
+        MazeGenerator gen = new MazeGenerator(dim-1);
         gen.generateMaze();
-        maze = gen.getIntMaze();
-    }
+        maze = load_walls(gen.getIntMaze(),dim);
 
+    }
+    static private int[][] load_walls(int[][] map , int dim){
+        int[][] maze = new int [dim][dim];
+        for(int i = 1 ; i < dim-1; i++ ){
+            for (int j = 1; j < dim-1;j++) {
+                if (map[i-1][j-1] != 0 )maze[i][j] = map[i-1][j-1];
+            }
+        }
+        return maze;
+    }
     public String stringMaze() {
         StringBuilder sb = new StringBuilder();
         for (int[] row : maze) {
@@ -19,11 +26,4 @@ public class Maze{
         }
         return sb.toString();
     }
-
-
 }
-
-
-
-
-
