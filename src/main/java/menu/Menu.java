b0a125d25a33a462d7be7f12707a0b6d278cd862
José Carvalho <z.m.f.carvalho@gmail.com>
2022-01-01 18:14:12 +0000

    Very basic version of the instructions menu

--- src/main/java/menu/Menu.java
+++ src/main/java/menu/Menu.java
@@ -4,8 +4,7 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
-import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.ActionListDialogBuilder;
 import com.googlecode.lanterna.screen.Screen;
 import element.stat.Wall;
@@ -16,11 +15,11 @@ import java.io.IOException;
 public class Menu implements MenuInterface {
     final private String backgroundcolor = "#000000";
     private Screen screen;
-    public Menu(Screen screen, int type ){
+    public Menu(Screen screen, int type ) throws IOException {
         this.screen = screen;
         draw(type);
     }
-    public void draw(int type) {
+    public void draw(int type) throws IOException {
         TextGraphics textgraphics = screen.newTextGraphics();
         textgraphics.setBackgroundColor(TextColor.Factory.fromString(backgroundcolor));
         textgraphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(200, 200), ' ');
@@ -80,24 +79,35 @@ public class Menu implements MenuInterface {
                         .showDialog(textGUI);
                 break;
             case 3:
-                new ActionListDialogBuilder()
-                        .setCanCancel(false)
-                        .setTitle("Instructions menu")
-                        .setDescription("                                                                                                                       ")
-                        .addAction("Instructions WIP", new Runnable() {
-                            @Override
-                            public void run() {
-                                Game.setState(1);
-                            }
-                        })
-                        .build()
-                        .showDialog(textGUI);
+                screen.startScreen();
+                Panel panel = new Panel();
+                panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+                panel.setPosition(new TerminalPosition(50,50));
+
+                final TextBox linha1 = new TextBox(new TerminalSize(30,5),"Instrucoes para o jogo \nVai do ponto a para o ponto B").addTo(panel);
+
+                linha1.setReadOnly(true);
+                linha1.setEnabled(false);
+
+                Button button = new Button("Back", new Runnable() {
+                    @Override
+                    public void run() {
+                        //TODO make this go to menu inicital
+                        System.out.println("MAKE ME GO BACKK!!!");
+                    }
+                });
+
+                button.addTo(panel);
+
+                panel.addComponent(new EmptySpace(new TerminalSize(0, 0)));
+                BasicWindow window = new BasicWindow();
+                window.setComponent(panel);
+
+                // Create gui and start gui
+                MultiWindowTextGUI gui1 = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+                gui1.addWindowAndWait(window);
                 break;
         }
-
-
-
-
-
+        }
     }
-}
+
