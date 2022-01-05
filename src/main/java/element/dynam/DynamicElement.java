28cd1eeaff5eb9cb1226b49ca08bb27298129f9e
José Carvalho <z.m.f.carvalho@gmail.com>
2022-01-05 17:02:58 +0000

    Added Documentation Dynamic element

--- src/main/java/element/dynam/DynamicElement.java
+++ src/main/java/element/dynam/DynamicElement.java
@@ -4,8 +4,21 @@ import com.googlecode.lanterna.SGR;
 import element.Element;
 import element.position.PositionInterface;
 
-public abstract class DynamicElement extends Element {
+/**
+ *  Class to contain and represent the dynamic elements of the game.
+ * @author Eduardo Correia
+ * @author Alberto Serra
+ * @author José Carvalho
+ */
 
+public abstract class DynamicElement extends Element {
+    /**
+     *  Constructor of the class DynamicElement
+      * @param position Position (x,y) to the element.
+     * @param color Color of the element.
+     * @param format format SGR.
+     * @param character Character to Represent the element.
+     */
     protected DynamicElement(PositionInterface position, String color, SGR format, String character) {
         super(position, color, format, character);
     }
