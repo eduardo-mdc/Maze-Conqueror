a7c4e925f8464e87dcd0ef05bd4cca11910cebfd
José Carvalho <z.m.f.carvalho@gmail.com>
2022-01-05 16:58:22 +0000

    Added more documentation

--- src/main/java/element/ElementInterface.java
+++ src/main/java/element/ElementInterface.java
@@ -3,38 +3,42 @@ package element;
 import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import element.position.PositionInterface;
-
+/**
+ * ElementInterface contains the functions set and get the basic components of the element.
+ *
+ * @author Eduardo Correia
+ * @author Alberto Serra
+ * @author José Carvalho
+ */
 public interface ElementInterface {
     /**
-     * Returns the color of the element
+     * Returns the color of the element.
      * @return
      */
     String getColor();
     /**
-     * Returns the format of the element
+     * Returns the format of the element.
      * @return
      */
     SGR getFormat();
     /**
-     * Returns the Character of the element
+     * Returns the Character of the element.
      * @return
      */
     String getCharacter();
     /**
-     * Returns the position of the element
+     * Returns the position of the element.
      * @return
      */
     PositionInterface getPosition();
-
     /**
-     * Sets the position of the element
-     * @param position Position (x,y) to set the element on
+     * Sets the position of the element.
+     * @param position Position (x,y) to set the element on.
      */
     void setPosition(PositionInterface position);
-
     /**
-     *  Draws the element in a given sreen
-     * @param screen Screen to draw de element
+     *  Draws the element in a given screen.
+     * @param screen Screen to draw the element on.
      */
     void draw(TextGraphics screen);
 }
--- src/main/java/element/position/PositionInterface.java
+++ src/main/java/element/position/PositionInterface.java
@@ -1,12 +1,34 @@
 package element.position;
+/**
+ * PositionInterface contains the functions to position the game elements relative to which other .
+ *
+ * @author Eduardo Correia
+ * @author Alberto Serra
+ * @author José Carvalho
+ */
 
 public interface PositionInterface {
-
+    /**
+     *  Returns the X position
+     * @return
+     */
     int getX();
 
+    /**
+     *  Sets the X position
+     * @param x x value to set
+     */
     void setX(int x);
 
+    /**
+     * Returns the Y position
+     * @return
+     */
     int getY();
 
+    /**
+     * Sets the Y position
+     * @param y y value to set
+     */
     void setY(int y);
 }
